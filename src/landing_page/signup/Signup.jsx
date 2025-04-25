6d14440ac75a885ac58828db3050d0df1b443844
Gaurav Jain <jaingaurav906@gmail.com>
2025-04-25 10:20:07 +0530

    Second Commit

--- src/landing_page/signup/Signup.jsx
+++ src/landing_page/signup/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import './SignUp.css';
+import './Signup.css';
 
 const SignUp = () => {
   const [formData, setFormData] = useState({
--- src/landing_page/signup/index.jsx
+++ src/landing_page/signup/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Login from './Login.jsx';
 import Signup from './Signup.jsx';
 
 
